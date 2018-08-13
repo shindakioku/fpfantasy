30c0cb52feb529b37f721f5f3acfb3bfb2eae300
shindakioku <shindakioku@gmail.com>
2018-08-13 14:44:43 +0300

    Fix curry generic types

--- lib/src/curry/curry.dart
+++ lib/src/curry/curry.dart
@@ -7,7 +7,7 @@ import './curry5.dart';
 Function curry(Function fn, {int argsLength: 2}) {
   assert(argsLength <= 5);
 
-  return <T>([T a, T b, T c, T d, T e]) {
+  return <T, E, A, B, C>([T a, E b, A c, B d, C e]) {
     var args = [a, b, c, d, e].where((a) => a != null);
 
     // If args is equal to length of function arguments
